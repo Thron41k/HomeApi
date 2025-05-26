0bcdd50de682fefe68a8b33716dbecc2c7b7050d
Timothy <Thron41k@gmail.com>
2025-05-26 16:04:34 +0800

    Добавьте файлы проекта.

---
+++ HomeApi/Startup.cs
@@ -0,0 +1,42 @@
+﻿using HomeApi.Configuration;
+using Microsoft.OpenApi.Models;
+
+namespace HomeApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; } = new ConfigurationBuilder().AddJsonFile("HomeOptions.json").Build();
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<HomeOptions>(Configuration);
+            // Нам не нужны представления, но в MVC бы здесь стояло AddControllersWithViews()
+            services.AddControllers();
+            // поддерживает автоматическую генерацию документации WebApi с использованием Swagger
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "HomeApi", Version = "v1" }); });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Проставляем специфичные для запуска при разработке свойства
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HomeApi v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            // Сопоставляем маршруты с контроллерами
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
